3d8b4711447d978123cbf897f5689471e3841b41
Megan O'Brien <megan.obrien3090@gmail.com>
2021-06-02 17:05:51 -0600

    Edit anagram.js to split, sort, and join the words
    Add if statement for comparing words

--- src/3-anagram/anagram.js
+++ src/3-anagram/anagram.js
@@ -1,5 +1,12 @@
 // Write a function that returns true if the two words are an anagram of eachother.
 // anagram: a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 exports.anagram = function (firstWord, secondWord) {
-  return false;
+  let a = firstWord.split("").sort().join("");
+  let b = secondWord.split("").sort().join("");
+
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 }
