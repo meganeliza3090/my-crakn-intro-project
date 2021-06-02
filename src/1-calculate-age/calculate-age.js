3eb309b22413ac30fdf8d8b768620fcbfb46cc0a
Megan O'Brien <megan.obrien3090@gmail.com>
2021-06-02 17:51:44 -0600

    Edit calculate-age.js to use built in Date object
    Add if statement for more complicated dates

--- src/1-calculate-age/calculate-age.js
+++ src/1-calculate-age/calculate-age.js
@@ -1,4 +1,12 @@
 // Write a function that returns the current age of the person in years.
 exports.ageAtDeath = function (birthDate, deathDate) {
-  return 0;
+  birthDate = new Date(birthDate);
+  deathDate = new Date(deathDate);
+
+  let age = (deathDate.getFullYear() - birthDate.getFullYear());
+
+  if (deathDate.getMonth() < birthDate.getMonth() || deathDate.getMonth() == birthDate.getMonth() && deathDate.getDate() < birthDate.getDate()) {
+    age--;
+  }
+  return age;
 }
